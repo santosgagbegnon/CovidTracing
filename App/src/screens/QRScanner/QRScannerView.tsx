532e9f7827d4d326653cb64a999d1e58f4ac6708
santosgagbegnon <santos.gagbegnon@shopify.com>
2021-03-27 15:47:49 -0400

    Add success and failure icons when logging customers

--- App/src/screens/QRScanner/QRScannerView.tsx
+++ App/src/screens/QRScanner/QRScannerView.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useMemo, useState, useLayoutEffect } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState, useRef } from "react";
+import { View, Text, StyleSheet, Animated } from "react-native";
 import { BarCodeScanner, BarCodeEvent } from "expo-barcode-scanner";
 import { useNavigation } from "@react-navigation/core";
 import { PrimaryButton } from "../BusinessProfile/components";
+import { successIcon } from "../../shared/images";
 
 const HEADER_HEIGHT = 75;
 
@@ -12,19 +13,15 @@ export const QRScannerView = () => {
   const [hasPermission, setHasPermission] = useState<boolean>();
   const [scanned, setScanned] = useState(false);
 
+  const sizeAnimation = useRef(new Animated.Value(30)).current;
+  const opacityAnimation = useRef(new Animated.Value(0)).current;
+
   useEffect(() => {
     (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
-      console.log(status);
-
       setHasPermission(status === "granted");
     })();
   });
-
-  const onBarCodeScanned = ({ type, data }: BarCodeEvent) => {
-    setScanned(true);
-    alert(`Bar code with type ${type} and data ${data}`);
-  };
 
   useEffect(() => {
     if (hasPermission === false) {
@@ -32,6 +29,50 @@ export const QRScannerView = () => {
       if (navigation.canGoBack()) navigation.goBack();
     }
   }, [hasPermission]);
+
+  const onBarCodeScanned = ({ type, data: uuid }: BarCodeEvent) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${uuid}`);
+    // Get customer info from users/uuid
+    // Log business informaiton in users/uuid (eg. visits.append({businessName, address timestmap}))
+    // Log customer informaiton in users/businessUUID (eg. visits.append({firstName, LastName, number, timestmap}))
+  };
+
+  const animateResult = () => {
+    const increaseSize = Animated.timing(sizeAnimation, {
+      useNativeDriver: false,
+      toValue: 100,
+      duration: 600,
+    });
+
+    const increaseOpacity = Animated.timing(opacityAnimation, {
+      useNativeDriver: false,
+      toValue: 1,
+      duration: 150,
+    });
+
+    const decreaseOpacity = Animated.timing(opacityAnimation, {
+      useNativeDriver: false,
+      toValue: 0,
+      duration: 600,
+    });
+
+    const staggeredOpacity = Animated.stagger(400, [
+      increaseOpacity,
+      decreaseOpacity,
+    ]);
+
+    const parallel = Animated.parallel([increaseSize, staggeredOpacity], {
+      stopTogether: false,
+    });
+
+    parallel.start((finish) => {
+      if (finish) {
+        sizeAnimation.setValue(30);
+        opacityAnimation.setValue(0);
+      }
+    });
+  };
 
   return (
     <View style={{ flex: 1 }}>
@@ -46,7 +87,20 @@ export const QRScannerView = () => {
               visit.
             </Text>
           </View>
-          <PrimaryButton title="Manually log customer" onPress={() => {}} />
+          <View style={styles.confirmationContainer}>
+            <Animated.Image
+              source={successIcon}
+              style={{
+                width: sizeAnimation,
+                height: sizeAnimation,
+                opacity: opacityAnimation,
+              }}
+            />
+          </View>
+          <PrimaryButton
+            title="Manually log customer"
+            onPress={animateResult}
+          />
         </View>
       </BarCodeScanner>
     </View>
@@ -72,5 +126,14 @@ const styles = StyleSheet.create({
   informationText: {
     color: "#fff",
     textAlign: "center",
+  },
+  confirmationContainer: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
